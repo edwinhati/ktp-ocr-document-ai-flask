60dc51153cedd4ce8affdd9fcea355f2e3174033
Edwin Hati <edwinhati@outlook.com>
2024-03-17 06:42:47 +0000

    Add Flask-Cors package and import CORS in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = f'credential.json'
 
 app = Flask(__name__)
+CORS(app)
 
 from app import views
 
